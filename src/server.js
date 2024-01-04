1a6555d9e09fdc57521dc768fad1c6fec6d000c4
Alfin <alfin.wijaya@agungsedayu.com>
2024-01-04 11:34:51 +0700

    2024-01-04

--- src/server.js
+++ src/server.js
@@ -3,12 +3,19 @@ require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
 const albums = require('./api/albums');
-const songs = require('./api/songs');
 const AlbumService = require('./services/postgres/AlbumsService');
-const SongService = require('./services/postgres/SongsService');
 const AlbumValidator = require('./validator/albums');
-const { SongValidator, QueryValidator } = require('./validator/songs');
 const ClientError = require('./exceptions/ClientError');
+
+// songs
+const songs = require('./api/songs');
+const SongService = require('./services/postgres/SongsService');
+const { SongValidator, QueryValidator } = require('./validator/songs');
+
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
   const albumService = new AlbumService();
@@ -24,22 +31,30 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: albums,
-    options: {
-      service: albumService,
-      validator: AlbumValidator,
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumService,
+        validator: AlbumValidator,
+      },
     },
-  });
-
-  await server.register({
-    plugin: songs,
-    options: {
-      service: songService,
-      validator: SongValidator,
-      queryValidator: QueryValidator,
+    {
+      plugin: songs,
+      options: {
+        service: songService,
+        validator: SongValidator,
+        queryValidator: QueryValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   server.ext('onPreResponse', (request, h) => {
     const { response } = request;
